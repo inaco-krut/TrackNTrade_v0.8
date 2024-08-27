4b10dfc0b1de494a4b51e39fb54f7a51c8895f77
inaco-krut <fredrik@krutech.se>
2024-08-28 01:41:59 +0200

    v.09 new links & added city/country names

--- src/main/java/org/example/MainFrame.java
+++ src/main/java/org/example/MainFrame.java
@@ -7,7 +7,9 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import javax.swing.*;
 import javax.swing.event.HyperlinkEvent;
 
@@ -17,10 +19,7 @@ public class MainFrame extends JFrame {
     private List<String> latestLinks; // added variable to store latest links
     private String latestQuery; // added variable to store latest query
 
-
     public MainFrame() {
-
-        //LoginScreen login = new LoginScreen(null);
 
         // main frame settings
         setTitle("Track Parts Finder For Goons");
@@ -30,11 +29,10 @@ public class MainFrame extends JFrame {
         setSize(420, 270);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-
         // TextField Settings
         textField = new JTextField();
         textField.setPreferredSize(new Dimension(200, 60));
-        textField.setFont(new Font("Consolas",Font.PLAIN,28));
+        textField.setFont(new Font("Consolas", Font.PLAIN, 28));
         textField.setBackground(Color.darkGray);
         textField.setForeground(Color.WHITE);
         textField.setCaretColor(Color.GREEN);
@@ -44,13 +42,13 @@ public class MainFrame extends JFrame {
         // URLButton settings
         JButton URLButton = new JButton("Click Here Or Press Enter");
         JButton openAllLinks = new JButton("Click Here To Open All Links In Browser");
-        URLButton.setFont(new Font("Consolas",Font.PLAIN,20));
+        URLButton.setFont(new Font("Consolas", Font.PLAIN, 20));
         URLButton.setBackground(Color.black); // add this line to change the background color
         URLButton.setForeground(Color.WHITE);
         openAllLinks.setBackground(Color.black);
         openAllLinks.setForeground(Color.WHITE);
 
-        // RadioButton settings
+        // Region RadioButton settings
         JRadioButton europeRadioButton = new JRadioButton("Europe");
         JRadioButton northAmericaRadioButton = new JRadioButton("North America");
         JRadioButton southEastAsiaRadioButton = new JRadioButton("Southeast Asia");
@@ -99,7 +97,7 @@ public class MainFrame extends JFrame {
                 return;
             }
 
-            // generate and display search queries // dataSets
+            // generate and display search queries
             String input = textField.getText();
             dataSetEurope dataSetEurope = new dataSetEurope(input);
             dataSetNA dataSetNA = new dataSetNA(input);
@@ -162,12 +160,9 @@ public class MainFrame extends JFrame {
         openAllLinks.addActionListener(e2 -> {
             for (String link : latestLinks) { // loop through latest links only
                 try {
-
                     Desktop.getDesktop().browse(new URI(link.startsWith("http") ? link : "http://" + link));
-
                 } catch (IOException | URISyntaxException ex) {
                     ex.printStackTrace();
-
                 }
             }
         });
@@ -176,16 +171,90 @@ public class MainFrame extends JFrame {
 
     private String getHtmlLinks(String queries, String userInput) {
         StringBuilder html = new StringBuilder();
+        if (queries == null || userInput == null) {
+            return ""; // Return empty string if input is null
+        }
+
+        // Set of European capital cities
+        Set<String> europeanCapitals = new HashSet<>(Arrays.asList(
+            "Amsterdam", "Andorra la Vella", "Athens", "Belgrade", "Berlin",
+            "Bratislava", "Brussels", "Bucharest", "Budapest", "Chisinau",
+            "Copenhagen", "Dodoma", "Helsinki", "Kiev", "Lisbon",
+            "Ljubljana", "London", "Luxembourg", "Madrid", "Minsk",
+            "Monte Carlo", "Moscow", "Nicosia", "Oslo", "Paris",
+            "Podgorica", "Prague", "Reykjavik", "Rome", "San Marino",
+            "Sarajevo", "Skopje", "Sofia", "Stockholm", "Tallinn",
+            "Tbilisi", "Tirana", "Vaduz", "Valletta", "Vatican City",
+            "Vienna", "Vilnius", "Warsaw", "Zagreb", "Zurich", "dublin", "riga"
+            
+        ));
+
         String[] links = queries.split("\n");
         for (String link : links) {
             try {
-                URI uri = new URI(link);
+                URI uri = new URI(link.trim()); // Trim to remove any leading/trailing spaces
                 String domain = uri.getHost();
                 if (domain != null) {
                     domain = domain.startsWith("www.") ? domain.substring(4) : domain;
-                    html.append("<a href=\"").append(link).append("\" style=\"color: blue; font-family: Helvetica, font-weight: plain;\">").append(domain).append("</a> ").append(userInput).append("<br>");
+
+                    // Determine display text
+                    String displayText = domain;
+                    
+                    // Check for Facebook URL and European capital
+                    if (domain.contains("facebook.com")) {
+                        String path = uri.getPath();
+                        if (path != null) {
+                            for (String capital : europeanCapitals) {
+                                if (path.toLowerCase().contains(capital.toLowerCase())) {
+                                    displayText = "Facebook.com - " + capital;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    
+                    // Check for specific TLDs and append the country name
+                    String country = "";
+                    if (domain.endsWith(".se")) {
+                        country = " - Sweden";
+                    } else if (domain.endsWith(".uk")) {
+                        country = " - United Kingdom";
+                    } else if (domain.endsWith(".de")) {
+                        country = " - Germany";
+                    } else if (domain.endsWith(".dk")) {
+                        country = " - Denmark";
+                    } else if (domain.endsWith(".fr")) {
+                        country = " - France";
+                    } else if (domain.endsWith(".no")) {
+                        country = " - Norway";
+                    } else if (domain.endsWith(".fi")) {
+                        country = " - Finland";
+                    } else if (domain.endsWith(".nl")) {
+                        country = " - Netherlands";
+                    } else if (domain.endsWith(".at")) {
+                        country = " - Austria";
+                    } else if (domain.endsWith(".hr")) {
+                        country = " - Croatia";
+                    } else if (domain.endsWith(".pl")) {
+                        country = " - Poland";
+                    } else if (domain.endsWith(".pt")) {
+                        country = " - Portugal";
+                    } else if (domain.endsWith(".ua")) {
+                        country = " - Ukraine";
+                    } else if (domain.endsWith(".bg")) {
+                        country = " - Bulgaria";
+                    } else if (domain.endsWith(".ch")) {
+                        country = " - Switzerland";
+                    }
+                    
+                    html.append("<a href=\"").append(link).append("\" style=\"color: blue; font-family: Helvetica; font-weight: normal;\">")
+                        .append(displayText).append(country)
+                        .append("</a> ")
+                        .append(userInput)
+                        .append("<br>");
                 }
             } catch (URISyntaxException e) {
+                // Log the error or skip the invalid URL
                 e.printStackTrace();
             }
         }
--- src/main/java/org/example/dataSetEurope.java
+++ src/main/java/org/example/dataSetEurope.java
@@ -11,27 +11,33 @@ public class dataSetEurope {
 
         StringBuilder queries = new StringBuilder();
 
-        String dbaQuery = "https://www.dba.dk/soeg/?soeg="+input;
+        String dbaQuery = "https://www.dba.dk/cykler/?soeg="+input;
         String mercariQuery = "https://www.mercari.com/search/?keyword="+input;
         String wallapopQuery = "https://uk.wallapop.com/app/search?keywords="+input+"&filters_source=search_box&latitude=51.509865&longitude=-0.118092";
-        String twoEmemainQuery = "https://www.2ememain.be/q/"+input+"/#Language:all-languages";
-        String finnNoQuery = "https://www.finn.no/bap/forsale/search.html?q="+input+"&sort=RELEVANCE";
-        String ebayKleinanzeigenQuery = "https://www.ebay-kleinanzeigen.de/s-"+input+"/k0";
-        String marktplaatsQuery = "https://www.marktplaats.nl/q/"+input+"/";
+        String finnNoQuery = "https://www.finn.no/bap/forsale/search.html?q="+input+"&sub_category=1.69.3963";
+        String ebayKleinanzeigenQuery = "https://www.kleinanzeigen.de/s-fahrraeder/"+input+"/k0c217";
+        String marktplaatsQuery = "https://www.marktplaats.nl/l/fietsen-en-brommers/#q:"+input;
         String bikemarktQuery = "https://bikemarkt.mtb-news.de/search/"+input;
         String trocVeloQuery = "https://www.troc-velo.com/fr-fr/recherche?search="+input;
-        String willhabenQuery = "https://www.willhaben.at/iad/kaufen-und-verkaufen/marktplatz?sfId=fe973a99-b481-431b-ab14-dcb3bbb3af03&isNavigation=true&keyword="+input;
-        String ricardoQuery = "https://www.ricardo.ch/de/s/"+input+"/";
-        String blocketQuery = "https://www.blocket.se/annonser/hela_sverige?q="+input;
-        String traderaQuery = "https://www.tradera.com/search?q="+input;
+        String willhabenQuery = "https://www.willhaben.at/iad/kaufen-und-verkaufen/marktplatz/fahrraeder-radsport-4525?sfId=c1892adb-f149-4fd1-9e83-f185016b0fef&rows=30&isNavigation=true&keyword="+input;
+        String ricardoQuery = "https://www.ricardo.ch/de/c/radsport-41950/"+input+"/";
         String happyRideQuery = "https://happyride.se/annonser/list.php?search="+input+"&category=&county=&creator=&type=";
-        String lebonCOinQuery = "https://www.leboncoin.fr/recherche?text="+input;
+        String lebonCOinQuery = "https://www.leboncoin.fr/recherche?category=55&text="+input;
+        String njuskaloCroatiaQuery = "https://www.njuskalo.hr/search/?keywords="+input;
+        String olxPolandQuery = "https://www.olx.pl/sport-hobby/rowery/q-"+input;
+        String olxPortugalQuery = "https://www.olx.pt/desporto-e-lazer/bicicletas/q-"+input;
+        String olxUkraineQuery = "https://www.olx.ua/uk/hobbi-otdyh-i-sport/velo/q-"+input+"/?currency=UAH";
+        String olxBulgariaQuery = "https://www.olx.bg/sport-knigi-hobi/sportni-stoki/velosipedi/q-"+input;
+        String anibisQuery = "https://www.anibis.ch/fr/q/velos/?sorting=newest&page=1&query="+input;
+        String milanuciosQuery = "https://www.milanuncios.com/bicicletas/?s="+input+"&demanda=n&vendedor=part&orden=relevance&fromSearch=1&fromSuggester=0&suggestionUsed=0&hitOrigin=listing&recentSearchShowed=0&recentSearchUsed=0";
+        String ToriQuery = "https://www.tori.fi/recommerce/forsale/search?q="+input+"&sub_category=1.69.3963";
+
+
 
 
         queries.append(dbaQuery.replace(" ","+")).append("\n");
         queries.append(mercariQuery.replace(" ","%20")).append("\n");
         queries.append(wallapopQuery.replace(" ", "%20")).append("\n");
-        queries.append(twoEmemainQuery.replace(" ", "+")).append("\n");
         queries.append(finnNoQuery.replace(" ", "+")).append("\n");
         queries.append(ebayKleinanzeigenQuery.replace(" ", "-")).append("\n");
         queries.append(marktplaatsQuery.replace(" ", "+")).append("\n");
@@ -39,10 +45,19 @@ public class dataSetEurope {
         queries.append(trocVeloQuery.replace(" ", "+")).append("\n");
         queries.append(willhabenQuery.replace(" ", "%20")).append("\n");
         queries.append(ricardoQuery.replace(" ", "%20")).append("\n");
-        queries.append(blocketQuery.replace(" ", "+")).append("\n");
-        queries.append(traderaQuery.replace(" ", "%20")).append("\n");
         queries.append(happyRideQuery.replace(" ", "+")).append("\n");
         queries.append(lebonCOinQuery.replace(" ","%20")).append("\n");
+        queries.append(olxPolandQuery.replace(" ","%20")).append("\n");
+        queries.append(olxPortugalQuery.replace(" ","%20")).append("\n");
+        queries.append(olxUkraineQuery.replace(" ","%20")).append("\n");
+        queries.append(anibisQuery.replace(" ","%20")).append("\n");
+        queries.append(milanuciosQuery.replace(" ","%20")).append("\n");
+        queries.append(ToriQuery.replace(" ","%20")).append("\n");
+        queries.append(olxBulgariaQuery.replace(" ","%20")).append("\n");
+        queries.append(njuskaloCroatiaQuery.replace(" ","%20")).append("\n");
+
+
+
 
         return queries.toString();
 
